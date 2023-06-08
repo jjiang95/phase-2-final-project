60eea6a15978620e704c6de71b350a20af79e062
Jasmine Jiang <jasminemazzarella@gmail.com>
2023-06-08 15:48:27 -0400

    adds fetch function to home page when it first loads

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,19 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 function Home() {
+
+    const [bookCollection, setBookCollection] = useState([])
+    useEffect(() => {
+        fetch("http://localhost:4000/books")
+        .then(resp => resp.json())
+        .then(books => setBookCollection(books)) 
+    },[])
+
     return (
         <div>
-            Home page
+            {bookCollection.map((book => (
+                <div key={book.id}>{book.title}</div>
+            )))}
         </div>
     )
 }
